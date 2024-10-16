e1cb3174dc4b9dcb909e5a2351422951729e2d23
preethiviy <preethivirajyambem@gmail.com>
2024-10-16 22:21:37 +0530

    game functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-import numpy as py
+import numpy as np
 
 pygame.init()
 
@@ -23,3 +23,81 @@ CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
 
 screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption('Tic Tac Toe AI')
+screen.fill(BLACK)
+
+board = np.zeros(BOARD_ROWS, BOARD_COLS)
+
+def draw_lines(color=WHITE):
+    for i in range(1, BOARD_ROWS):
+        pygame.draw.line(
+            screen, 
+            color, 
+            (0, SQUARE_SIZE * i), 
+            (WIDTH, SQUARE_SIZE * i),
+            LINE_WIDTH
+        )
+        pygame.draw.line(
+            screen,
+            color,
+            (SQUARE_SIZE * i, 0),
+            (SQUARE_SIZE * i, HEIGHT),
+            LINE_WIDTH
+        )
+
+def draw_figures(color=WHITE):
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(
+                    screen,
+                    color,
+                    (int(col * SQUARE_SIZE + SQUARE_SIZE // 2), int(row * SQUARE_SIZE + SQUARE_SIZE // 2)),
+                    CIRCLE_RADIUS,
+                    CIRCLE_WIDTH
+                )
+            elif board[row][col] == 2:
+                pygame.draw.line(
+                    screen,
+                    color,
+                    (col * SQUARE_SIZE + SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4),
+                    (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4),
+                    LINE_WIDTH
+                )
+                pygame.draw.line(
+                    screen,
+                    color,
+                    (col * SQUARE_SIZE + SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4),
+                    (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4),
+                    LINE_WIDTH
+                )
+
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def available_square(row, col):
+    return board[row][col] == 0
+
+def is_board_available(check_board=board):
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if check_board[row][col] == 0:
+                return True
+    return False
+
+def check_win(player, check_board=board):
+    for col in range(BOARD_COLS):
+        if check_board[0][col] == player and check_board[1][col] == player and check_board[2][col] == player:
+            return True
+        
+    for row in range(BOARD_ROWS):
+        if check_board[row][0] == player and check_board[row][1] == player and check_board[row][2] == player:
+            return True
+        
+    if check_board[0][0] == player and check_board[1][1] == player and check_board[2][2] == player:
+        return True
+    
+    if check_board[0][2] == player and check_board[1][1] == player and check_board[2][0] == player:
+        return True
+    
+    return False
